4cd54b494a257b5d7eb4567c4ba81d49e11e82b9
CaitlinnBrown <91679692+CaitlinnBrown@users.noreply.github.com>
2022-05-11 22:15:34 +0100

    Update NewITReturnpage.js

--- pages/NewITReturnpage.js
+++ pages/NewITReturnpage.js
@@ -7,97 +7,128 @@ Reviewed by: Caitlin Brown
 Review date: 2022/05/11
 
 Modification history -
-
 Description:
+1) Page reconfigured to reflect return journey - walk placed at start of 
+each item and castle and original departure location reversed. Price 
+and map removed as already displayed to user
 
 Date:
+1) 11/05/2022
 
 */
-
-
-
-
-
 
 
 import React from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity ,Button} from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, View, Text, ScrollView, TouchableOpacity } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
-import { Center } from 'native-base';
-
-
 
 
 /*
-The method of the internary page, by allowing the user to view the specific transfer information of this trip.
+The method of the internary page, by allowing the user to view the specific transfer information of return trip.
+Takes navigation as parameter to aid with stack navigation. Route parameter used to obtain variables
+from previous page
 */
-export default function internaryReturn({ navigation }) {
- 
- 
-    /*List all the data in the itinerary, you can connect to the database from here. 
-      After receiving the data from the database, the parameters will be passed to the specific method for rendering the front-end page*/
- 
-    const [journeys] = useState([
-        { DepartTime: '09:17',
-         arriveTime: '10:24',
-         DepartLocation: 'Nupermarket', 
-         ArriveLocation: 'Newcastle castle',
-         jourLength: '1hr 22mins', 
-         changes: '0', 
-         busID: 'IC108', 
-         Destination: 'London Center', 
-         departs: ' Haymarket Bus Station', 
-         platformStart: '7', 
-         platformEnd: '3', 
-         key: '1' },
-         { DepartTime: '09:17',
-         arriveTime: '10:24',
-         DepartLocation: 'Nupermarket', 
-         ArriveLocation: 'Newcastle castle',
-         jourLength: '1hr 22mins', 
-         changes: '0', 
-         busID: 'IC108', 
-         Destination: 'London Center', 
-         departs: ' Haymarket Bus Station', 
-         platformStart: '7', 
-         platformEnd: '3', 
-         key: '2' },
-  
-      ]);
- /*Returns the specific rendering of the page, using different containers and 
-      flatlists to render the different transfer parts formed this time*/
-  
+export default function internary({ navigation, route }) {
+      /*Objects from item of FlatList selected */
+      const { castle, DepartTime, arriveTime, jourLength, changes, day,
+        bus, operator, departs, walkTime, walkDistance, changeOver1Arrive, changeOver1Departs,
+        changeOver2Arrive, changeOver2Departs, jour1Length, jour2Length, jour3Length, bus2,
+        operator2, bus3, operator3, busPrice, busPrice2, castlePrice, changeOverArrStop,
+        changeOverDepStop, changeOverArrStop2, changeOverDepStop2, departStop } = route.params;
+   
+
+        /*
+        Display chnages function displays information about bus stops, buses, operators and times depending on
+        number of change overs required.
+        */
+        const displayChanges = () => {
+            if(changes === '0') {
+                return(
+                <View style={styles.container}>
+                    <Text style={styles.titleText}>{ walkDistance } {'\n'}{ walkTime } {'\n'}{'\n'}{ departStop } </Text>
+                    <FontAwesome5 style={styles.walking} name="walking" size={24} color="black"  />
+
+                    <View style={styles.line} ></View>
+                    <Text style={styles.titleText}>{ DepartTime } { departStop }  {'\n'}{'\n'}{'\n'}{ arriveTime }  { departs } {'\n'}</Text>
+                    <Ionicons name="bus-sharp" size={34} color="black" />
+                    <Text style={styles.text}> Bus: { bus } | Operator: {operator} {'\n'} {jour1Length} {'\n'}</Text>
+                    <View style={styles.line} ></View>
+
+                </View>
+                )
+            }
+            if(changes === '1'){
+                return (
+                <View style={styles.container}>
+
+<                   Text style={styles.titleText}>{ walkDistance } {'\n'}{ walkTime } {'\n'}{'\n'}{ departStop } </Text>
+                    <FontAwesome5 style={styles.walking} name="walking" size={24} color="black"  />
+
+<                   View style={styles.line} ></View>
+                    <Text style={styles.titleText}>{ DepartTime } { departStop }  {'\n'}{'\n'}{'\n'}{ changeOver1Arrive }  { changeOverArrStop } {'\n'}</Text>
+                    <Ionicons name="bus-sharp" size={34} color="black" />
+                    <Text style={styles.text}> Bus: { bus } | Operator: {operator} {'\n'} {jour1Length} {'\n'}</Text>
+                    <View style={styles.line} ></View>
+
+                    <Text style={styles.titleText}>{ changeOver1Departs } { changeOverDepStop }  {'\n'}{'\n'}{'\n'}{ arriveTime } { departs } {'\n'}</Text>
+                    <Ionicons name="bus-sharp" size={34} color="black" />
+                    <Text style={styles.text}> Bus: { bus2 } | Operator: {operator2} {'\n'} {jour2Length} {'\n'}</Text>
+                    <View style={styles.line} ></View>
+
+                </View>
+                )
+            } else if (changes === '2') {
+                return (
+                <View style={styles.container}>
+                    <Text style={styles.titleText}>{ walkDistance } {'\n'}{ walkTime } {'\n'}{'\n'}{ departStop } </Text>
+                    <FontAwesome5 style={styles.walking} name="walking" size={24} color="black"  />
+
+<                   View style={styles.line} ></View>
+                    <Text style={styles.titleText}>{ DepartTime } { departStop }  {'\n'}{'\n'}{'\n'}{ changeOver1Arrive }  { changeOverArrStop } {'\n'}</Text>
+                    <Ionicons name="bus-sharp" size={34} color="black" />
+                    <Text style={styles.text}> Bus: { bus } | Operator: {operator} {'\n'} {jour1Length} {'\n'}</Text>
+                    <View style={styles.line} ></View>
+
+                    <Text style={styles.titleText}>{ changeOver1Departs } { changeOverDepStop }  {'\n'}{'\n'}{'\n'}{ changeOver2Arrive }  { changeOverArrStop2 } {'\n'}</Text>
+                    <Ionicons name="bus-sharp" size={34} color="black" />
+                    <Text style={styles.text}> Bus: { bus2 } | Operator: {operator2} {'\n'} {jour2Length} {'\n'}</Text>
+                    <View style={styles.line} ></View>
+     
+                    <Text style={styles.titleText}>{ changeOver2Departs } { changeOverDepStop2 }  {'\n'}{'\n'}{'\n'}{ arriveTime } { departs }         {'\n'}</Text>
+                    <Ionicons name="bus-sharp" size={34} color="black" />
+                    <Text style={styles.text}> Bus: { bus3 } | Operator: {operator3} {'\n'} {jour3Length} {'\n'}</Text>
+                    <View style={styles.line} ></View>
+
+
+                </View>
+                )
+            }
+        }
+
+
+
+      /*Returns the specific rendering of the page, using different containers to render the different transfer parts formed this time*/
       return (
         <View style={styles.container}>
             <View style={styles.topslide}></View>
             <View style={styles.title}>
-                <Text style={styles.titleText}>Today, 01-05-2022{"\n"}  Nupermarket - Newcastle castle </Text> 
-                <Text style={styles.text}> Duration: 1:10 | 0 change </Text>
+                {/* Header for the page displaying departure location, castke, total journey length */}
+                <Text style={styles.titleText}>{castle} Castle - {departs} </Text> 
+                <Text style={styles.text}> Duration: {jourLength} | Changes: {changes} </Text>
             </View>
-
-          <FlatList data={journeys} renderItem={({ item }) => (
-            
+            {/* ScrollView component enables user to scroll through information while keeping header and button
+            fixed in place */}
+            <ScrollView>
                 <View style={styles.journeyBox}>
-                    <Text style={styles.titleText}>{ item.DepartTime }        { item.DepartLocation }  {'\n'}{'\n'}{'\n'}{ item.arriveTime }        { item.ArriveLocation } {'\n'}</Text>
-                    <Text style={styles.titleText}>Pl. { item.platformStart }{'\n'}{'\n'}{'\n'}Pl. { item.platformEnd }</Text>
-
-                    <View style={styles.line} ></View>
-                    <Ionicons name="bus-sharp" size={34} color="black" />
-                    <Text style={styles.text}> Bus：{ item.busID } {'\n'}To: { item.Destination }{'\n'}</Text>
-                    <View style={styles.walking}>
-                    
-                    </View>
-
+                    {displayChanges()}
                 </View>
-        
-          )} />
-          
+            </ScrollView>
            <View style={styles.rect6}>
-                              {/*The confirmation button confirms this formation after confirming the data, click the next page of the reaction stack */}
-
-        <TouchableOpacity style={styles.button}   onPress={() => navigation.navigate('Order Confirm')}>
+               {/*The confirmation button confirms this formation after confirming the data, click the next page of the reaction stack */}
+        <TouchableOpacity style={styles.button}  
+             onPress={() => navigation.navigate('Order Confirm', {busPrice, busPrice2, castlePrice, castle})}
+             >
           <Text style={styles.confirmtext}>Confirm</Text>
         </TouchableOpacity>
            </View>
@@ -108,11 +139,15 @@ export default function internaryReturn({ navigation }) {
 
 
 
-/*style file for this page*/
+
+/*
+style file for this page
+*/
 const styles = StyleSheet.create ({
     container: {
         flex: 1,
-        padding: 5
+        padding: 5,
+        flexDirection: 'column'
     },
     title: {
         padding: 5,
@@ -155,7 +190,9 @@ const styles = StyleSheet.create ({
         marginTop: 5
     },
     walking: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        padding: 5 
+        
 
     },
     rect6: {
